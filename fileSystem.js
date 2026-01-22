217fdbff018b764c8cabcb1959530394a06fc5e2
daksh gupta <168224098+daksh1gupta@users.noreply.github.com>
2026-01-22 15:18:50 +0530

    Add Promises

--- fileSystem.js
+++ fileSystem.js
@@ -1,16 +1,42 @@
-const fs = require("fs");
-// fs.writeFile("daksh.txt", "Today we re learning file system",(error)=>{
+const fs = require("fs").promises;
+const {promisify} = require("util");
+const readFileAsync = promisify(require('fs').readFile);
+
+
+fs.writeFile("daksh.txt", "Today we re learning file system",(error)=>{
+    if(error){
+        console.log("Error is creating file")
+    }else{
+        console.log("File is created")
+    }
+})
+
+// fs.readFile("daksh.txt","utf-8",(error,data)=>{
 //     if(error){
-//         console.log("Error is creating file")
+//         console.log("Error is loading")
 //     }else{
-//         console.log("File is created")
+//         console.log("File Data: ",data)
 //     }
 // })
 
-fs.readFile("daksh.txt","utf-8",(error,data)=>{
-    if(error){
-        console.log("Error is loading")
-    }else{
-        console.log("File Data: ",data)
+async function readFile(){
+    try{
+        const data = await fs.readFile("daksh.txt", "utf-8")
+        console.log("File Content", data)
+    }catch (error){
+        console.error("Error in files content" , error)
     }
-})
+}
+
+readFile();
+
+async function readWithPromisify(){
+    try{
+        const data = await readFileAsync("daksh.txt", "utf-8");
+        console.log(data)
+    }catch(error){
+        console.log(error);
+    }
+}
+
+readWithPromisify();
