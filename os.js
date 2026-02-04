2de31d969da9d9d3bdfd057f995d351847b0509c
daksh gupta <168224098+daksh1gupta@users.noreply.github.com>
2026-02-04 14:46:59 +0530

    make OS.js File

---
+++ os.js
@@ -0,0 +1,9 @@
+// iss se sirf mcq hi aayega 
+
+
+const os = require('os');
+
+console.log(os.platform());       //win32
+console.log(os.arch());         //x64
+console.log(os.cpus());
+console.log(os.totalmem());     //total ram memory
